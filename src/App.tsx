6cd8e7f0aed06af5d5b1bfdd858822c7758aa056
Brian Marete <bmarete10@gmail.com>
2024-09-25 13:22:15 +0300

    Fix mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,44 @@ import flowing_conversation_image from "./assets/images/illustration-flowing-con
 import your_users_image from "./assets/images/illustration-your-users.svg";
 import communities_icon from "./assets/images/icon-communities.svg";
 import messages_icon from "./assets/images/icon-messages.svg";
+import { MdEmail, MdPhoneInTalk } from "react-icons/md";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagram, FaSquareTwitter } from "react-icons/fa6";
 
 export default function HuddleLandingPage() {
   return (
     <div className="min-h-screen flex flex-col">
       <header className="container mx-auto px-4 py-6 flex justify-between items-center">
-        <div className="flex items-center space-x-2">
-          <img src={logo} />
+        <div>
+          <img src={logo} className="w-2/3 md:w-full" />
         </div>
-        <button className="rounded-full text-pink border-2 border-pink py-2 px-6">
+        <button className="rounded-full text-pink border-2 border-pink py-1 md:py-2 px-3 md:px-6 text-xs md:text-base">
           Try it Free
         </button>
       </header>
 
       <main className="flex-grow py-12">
-        <section className="text-center space-y-6">
-          <h1 className="text-5xl font-poppins font-bold text-gray-800">
+        <section className="text-center space-y-6 my-8 sm:mx-4">
+          <h1 className="text-3xl md:text-5xl font-poppins font-bold text-gray-800 leading-10 mx-8">
             Build The Community Your Fans Will Love
           </h1>
-          <p className="md:w-1/3 mx-auto text-gray-600 leading-7">
+          <p className="md:mx-auto mx-8 max-w-prose text-gray-600 leading-7">
             Huddle re-imagines the way we build communities. You have a voice,
             but so does your audience. Create connections with your users as you
             engage in genuine discussion.
           </p>
-          <button className="rounded-full drop-shadow-lg font-semibold bg-pink hover:bg-pink text-white py-4 px-20">
+          <button className="rounded-full drop-shadow-lg font-semibold bg-pink hover:bg-pink text-white py-2 px-10 md:py-4 md:px-20 text-sm md:text-base">
             Get Started For Free
           </button>
         </section>
 
-        <img src={screen_mockups} alt="" className="w-4/6 h-auto mx-auto" />
+        <img
+          src={screen_mockups}
+          alt=""
+          className="w-4/6 h-auto mx-auto md:my-8 my-32"
+        />
 
-        <section className="flex justify-around text-center">
+        <section className="flex md:flex-row flex-col items-center justify-around text-center my-8 space-y-6">
           <div>
             <img src={communities_icon} alt="" />
             <h2 className="text-7xl font-poppins font-bold text-gray-800 my-3">
@@ -53,8 +60,8 @@ export default function HuddleLandingPage() {
           </div>
         </section>
 
-        <div className="w-full h-44 bg-section-top-desktop-1 bg-cover"></div>
-        <section className="flex flex-col md:flex-row items-center gap-12 bg-pale-blue py-8 px-32">
+        <div className="w-full md:h-44 h-24 bg-section-top-mobile-1 md:bg-section-top-desktop-1 md:bg-cover"></div>
+        <section className="flex flex-col md:flex-row items-center gap-12 bg-pale-blue md:py-8 py-4 md:px-32 px-8 text-center md:text-left">
           <div className="md:w-1/2">
             <h2 className="text-3xl font-bold text-gray-800 mb-4">
               Grow Together
@@ -65,7 +72,7 @@ export default function HuddleLandingPage() {
               miss out on with a feedback form.
             </p>
           </div>
-          <div className="md:w-1/2">
+          <div className="md:w-1/2 order-first md:order-last">
             <img
               src={grow_together_image}
               alt="Grow together illustration"
@@ -73,8 +80,8 @@ export default function HuddleLandingPage() {
             />
           </div>
         </section>
-        <div className="w-full h-36 bg-section-bottom-desktop-1 bg-cover"></div>
-        <section className="flex flex-col md:flex-row-reverse items-center gap-12 py-8 px-32">
+        <div className="w-full h-36 bg-section-bottom-mobile-1 md:bg-section-bottom-desktop-1 bg-cover"></div>
+        <section className="flex flex-col md:flex-row-reverse items-center gap-12 md:py-8 py-4 md:px-32 px-8 text-center md:text-left">
           <div className="md:w-1/2">
             <h2 className="text-3xl font-bold text-gray-800 mb-4">
               Flowing Conversations
@@ -85,7 +92,7 @@ export default function HuddleLandingPage() {
               flow.
             </p>
           </div>
-          <div className="md:w-1/2">
+          <div className="md:w-1/2 order-first md:order-last">
             <img
               src={flowing_conversation_image}
               alt="Flowing conversations illustration"
@@ -94,8 +101,8 @@ export default function HuddleLandingPage() {
           </div>
         </section>
 
-        <div className="w-full h-44 bg-section-top-desktop-1 bg-cover"></div>
-        <section className="flex flex-col md:flex-row items-center gap-12 bg-pale-blue py-8 px-32">
+        <div className="w-full h-44 bg-section-top-mobile-1 md:bg-section-top-desktop-1 bg-cover"></div>
+        <section className="flex flex-col md:flex-row items-center gap-12 bg-pale-blue md:py-8 py-4 md:px-32 px-8 text-center md:text-left">
           <div className="md:w-1/2">
             <h2 className="text-3xl font-bold text-gray-800 mb-4">
               Your Users
@@ -106,7 +113,7 @@ export default function HuddleLandingPage() {
               your users can start chatting immediately.
             </p>
           </div>
-          <div className="md:w-1/2">
+          <div className="md:w-1/2 order-first md:order-last">
             <img
               src={your_users_image}
               alt="Your users illustration"
@@ -116,7 +123,7 @@ export default function HuddleLandingPage() {
             />
           </div>
         </section>
-        <div className="w-full h-36 bg-section-bottom-desktop-2 bg-cover"></div>
+        <div className="w-full h-36 bg-section-bottom-mobile-2 md:bg-section-bottom-desktop-2 bg-cover"></div>
 
         <section className="h-96 flex flex-col justify-center">
           <div className="text-center">
@@ -130,9 +137,9 @@ export default function HuddleLandingPage() {
         </section>
       </main>
 
-      <div className="w-full h-44 bg-footer-top-desktop bg-cover"></div>
-      <footer className="bg-dark-cyan text-white py-12">
-        <div className="container mx-auto px-4 grid md:grid-cols-2 gap-8">
+      <div className="w-full md:h-44 h-11 bg-footer-top-mobile md:bg-footer-top-desktop bg-[length:100%] bg-no-repeat"></div>
+      <footer className="bg-dark-cyan text-white py-20">
+        <div className="container mx-auto px-4 flex justify-between flex-col md:flex-row gap-32">
           <div className="space-y-4">
             <div className="flex items-center space-x-2">
               <img src={white_logo} />
@@ -141,69 +148,43 @@ export default function HuddleLandingPage() {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
               nulla quam, hendrerit lacinia vestibulum a, ultrices quis sem.
             </p>
-            <p>Phone: +1-543-123-4567</p>
-            <p>example@huddle.com</p>
+            <p>
+              <MdPhoneInTalk className="inline mr-2 text-xl" /> Phone:
+              +1-543-123-4567
+            </p>
+            <p>
+              <MdEmail className="inline mr-2 text-xl" />
+              example@huddle.com
+            </p>
             <div className="flex space-x-4">
               <a href="#" className="text-white hover:text-pink-500">
                 <span className="sr-only">Facebook</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <FaFacebookSquare className="text-3xl" />
               </a>
               <a href="#" className="text-white hover:text-pink-500">
                 <span className="sr-only">Instagram</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <FaInstagram className="text-3xl" />
               </a>
               <a href="#" className="text-white hover:text-pink-500">
                 <span className="sr-only">Twitter</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                >
-                  <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                </svg>
+                <FaSquareTwitter className="text-3xl" />
               </a>
             </div>
           </div>
-          <div>
+          <div className="order-first md:order-last">
             <h3 className="text-lg font-semibold mb-4">NEWSLETTER</h3>
             <p className="mb-4">
               To recieve tips on how to grow your community, sign up to our
               weekly newsletter. We'll never send you spam or pass on your email
               address
             </p>
-            <form className="flex gap-2">
+            <form className="flex md:flex-row flex-col md:gap-12 gap-2">
               <input
                 type="email"
-                placeholder="Email address"
-                className="rounded-full"
+                placeholder="Email"
+                className="rounded-sm py-2 px-2 text-gray-900"
               />
-              <button
-                type="submit"
-                className="rounded-full bg-pink-500 hover:bg-pink-600"
-              >
+              <button type="submit" className="rounded-sm bg-pink py-2 px-8">
                 Subscribe
               </button>
             </form>
